e31e3e723656a1cc74a28b75fd6cb6808f198f59
Dennis Iversen <deiv@aarhus.dk>
2025-04-28 11:38:31 +0200

    rename script name

---
+++ bin/check-urls.js
@@ -0,0 +1,56 @@
+import { existingUrls } from './utils.js';
+import { config } from './config.js';
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function checkUrls() {
+    const failedUrls = [];
+
+    for (const url of existingUrls) {
+        try {
+            const response = await fetch(url);
+            if (response.ok) {
+                console.log(`${url}: OK`);
+            } else {
+                console.log(`${url}: Error (${response.status})`);
+                failedUrls.push(url);
+            }
+        } catch {
+            console.log(`${url}: Error (Network Error)`);
+            failedUrls.push(url);
+        }
+
+        await sleep(1000);
+    }
+
+    return failedUrls;
+};
+
+async function sendMattermostMessage(message) {
+    const mattermostWebhookUrl = config.mattermostWebhookUrl;
+    const payload = { text: message };
+    const headers = { 'Content-Type': 'application/json' };
+
+    try {
+        const response = await fetch(mattermostWebhookUrl, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(payload),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to send message: ${response.statusText}`);
+        }
+    } catch (error) {
+        console.error(`Failed to send message: ${error}`);
+    }
+}
+
+const failedUrls = await checkUrls();
+
+if (failedUrls.length > 0) {
+    // const mattermostWebhookUrl = 'https://your-mattermost-webhook-url';
+    // const subject = 'Failed URLs';
+    const message = `The following URLs failed:\n${failedUrls.join('\n')}`;
+    await sendMattermostMessage(message);
+}
