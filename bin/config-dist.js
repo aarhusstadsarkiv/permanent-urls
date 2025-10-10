e4c18bd12e2bb0a68e71cb71f8b7fdea9db0115e
Dennis Iversen <deiv@aarhus.dk>
2025-10-10 13:03:21 +0200

    rm bin dir

--- bin/config-dist.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    mattermostWebhookUrl: "https://website.dk/hooks/identifier",
-}
-
-export { config };
--- bin/random.js
+++
@@ -1,5 +0,0 @@
-import { generateRandomString } from './utils.js';
-
-// out random string added .html extension
-let fileName = generateRandomString() + '.html'
-console.log(fileName)
