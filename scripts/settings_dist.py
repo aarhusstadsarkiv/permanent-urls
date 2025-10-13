0a34674316bb916ed3fcdf1c156f9f321142d997
Dennis Iversen <deiv@aarhus.dk>
2025-10-13 10:36:36 +0200

    update mattermost settings

--- scripts/settings_dist.py
+++ scripts/settings_dist.py
@@ -1,1 +1,6 @@
-MATTERMOST_WEBHOOK_URL="https://chat.openaws.dk/hooks/identifier-here"
+# Mattermost specific
+MATTERMOST = {
+    "base_url": "https://your-mattermost-instance.com",
+    "token": "your-token-here",
+    "channel_id": "your-channel-id-here",
+}
